610c788aff243cd986608818b128620b54861c22
mrs-destroyer <154685685+mrs-destroyer@users.noreply.github.com>
2024-03-28 23:22:37 +0500

    Create task.py

---
+++ task.py
@@ -0,0 +1,328 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(331, 395)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../../../../Downloads/download.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Form.setWindowIcon(icon)
+        Form.setStyleSheet("background-color:black")
+        self.pushButton_32 = QtWidgets.QPushButton(Form)
+        self.pushButton_32.setGeometry(QtCore.QRect(10, 330, 71, 51))
+        self.pushButton_32.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_32.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_32.setObjectName("pushButton_32")
+        self.pushButton_39 = QtWidgets.QPushButton(Form)
+        self.pushButton_39.setGeometry(QtCore.QRect(90, 330, 71, 51))
+        self.pushButton_39.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_39.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_39.setObjectName("pushButton_39")
+        self.pushButton_38 = QtWidgets.QPushButton(Form)
+        self.pushButton_38.setGeometry(QtCore.QRect(170, 330, 71, 51))
+        self.pushButton_38.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_38.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_38.setObjectName("pushButton_38")
+        self.pushButton_33 = QtWidgets.QPushButton(Form)
+        self.pushButton_33.setGeometry(QtCore.QRect(10, 270, 71, 51))
+        self.pushButton_33.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"") 
+        self.pushButton_33.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_33.setObjectName("pushButton_33")
+        self.pushButton_40 = QtWidgets.QPushButton(Form)
+        self.pushButton_40.setGeometry(QtCore.QRect(90, 270, 71, 51))
+        self.pushButton_40.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_40.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_40.setObjectName("pushButton_40")
+        self.pushButton_41 = QtWidgets.QPushButton(Form)
+        self.pushButton_41.setGeometry(QtCore.QRect(170, 270, 71, 51))
+        self.pushButton_41.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_41.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_41.setObjectName("pushButton_41")
+        self.pushButton_34 = QtWidgets.QPushButton(Form)
+        self.pushButton_34.setGeometry(QtCore.QRect(10, 210, 71, 51))
+        self.pushButton_34.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_34.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_34.setObjectName("pushButton_34")
+        self.pushButton_42 = QtWidgets.QPushButton(Form)
+        self.pushButton_42.setGeometry(QtCore.QRect(90, 210, 71, 51))
+        self.pushButton_42.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_42.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_42.setObjectName("pushButton_42")
+        self.pushButton_43 = QtWidgets.QPushButton(Form)
+        self.pushButton_43.setGeometry(QtCore.QRect(170, 210, 71, 51))
+        self.pushButton_43.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_43.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_43.setObjectName("pushButton_43")
+        self.pushButton_35 = QtWidgets.QPushButton(Form)
+        self.pushButton_35.setGeometry(QtCore.QRect(10, 150, 71, 51))
+        self.pushButton_35.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_35.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_35.setObjectName("pushButton_35")
+        self.pushButton_44 = QtWidgets.QPushButton(Form)
+        self.pushButton_44.setGeometry(QtCore.QRect(90, 150, 71, 51))
+        self.pushButton_44.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_44.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_44.setObjectName("pushButton_44")
+        self.pushButton_45 = QtWidgets.QPushButton(Form)
+        self.pushButton_45.setGeometry(QtCore.QRect(170, 150, 71, 51))
+        self.pushButton_45.setStyleSheet("background-color:rgb(56, 56, 56);\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_45.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_45.setObjectName("pushButton_45")
+        self.pushButton_46 = QtWidgets.QPushButton(Form)
+        self.pushButton_46.setGeometry(QtCore.QRect(90, 90, 71, 51))
+        self.pushButton_46.setStyleSheet("background-color:#242629;\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_46.setObjectName("pushButton_46")
+        self.pushButton_36 = QtWidgets.QPushButton(Form)
+        self.pushButton_36.setGeometry(QtCore.QRect(250, 90, 71, 51))
+        self.pushButton_36.setStyleSheet("background-color:#242629;\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_36.setObjectName("pushButton_36")
+        self.pushButton_37 = QtWidgets.QPushButton(Form)
+        self.pushButton_37.setGeometry(QtCore.QRect(170, 90, 71, 51))
+        self.pushButton_37.setStyleSheet("background-color:#242629;\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_37.setObjectName("pushButton_37")
+        self.pushButton_30 = QtWidgets.QPushButton(Form)
+        self.pushButton_30.setGeometry(QtCore.QRect(10, 90, 71, 51))
+        self.pushButton_30.setStyleSheet("background-color:#242629;\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_30.setIconSize(QtCore.QSize(16, 16))
+        self.pushButton_30.setObjectName("pushButton_30")
+        self.pushButton_47 = QtWidgets.QPushButton(Form)
+        self.pushButton_47.setGeometry(QtCore.QRect(250, 150, 71, 51))
+        self.pushButton_47.setStyleSheet("background-color:#242629;\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_47.setObjectName("pushButton_47")
+        self.pushButton_48 = QtWidgets.QPushButton(Form)
+        self.pushButton_48.setGeometry(QtCore.QRect(250, 210, 71, 51))
+        self.pushButton_48.setStyleSheet("background-color:#242629;\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_48.setObjectName("pushButton_48")
+        self.pushButton_49 = QtWidgets.QPushButton(Form)
+        self.pushButton_49.setGeometry(QtCore.QRect(250, 270, 71, 51))
+        self.pushButton_49.setStyleSheet("background-color:#242629;\n"
+"color:white;\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_49.setObjectName("pushButton_49")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(6, 9, 321, 71))
+        self.label.setStyleSheet("color:rgb(255, 255, 255);\n"
+"font: 25 28pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.label.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.label.setObjectName("label")
+        self.pushButton_50 = QtWidgets.QPushButton(Form)
+        self.pushButton_50.setGeometry(QtCore.QRect(250, 330, 71, 51))
+        self.pushButton_50.setStyleSheet("background-color:rgb(129, 210, 216);\n"
+"color:rgb(36, 58, 60);\n"
+"border-radius:6px;\n"
+"font: 25 18pt \"Bahnschrift Light Condensed\";\n"
+"")
+        self.pushButton_50.setObjectName("pushButton_50")
+        
+        self.pushButton_38.clicked.connect(self.point)
+        self.pushButton_39.clicked.connect(self.num0)
+        self.pushButton_37.clicked.connect(self.num00)
+        self.pushButton_33.clicked.connect(self.num1)
+        self.pushButton_40.clicked.connect(self.num2)
+        self.pushButton_41.clicked.connect(self.num3)
+        self.pushButton_34.clicked.connect(self.num4)
+        self.pushButton_42.clicked.connect(self.num5)
+        self.pushButton_43.clicked.connect(self.num6)
+        self.pushButton_35.clicked.connect(self.num7)
+        self.pushButton_44.clicked.connect(self.num8)
+        self.pushButton_45.clicked.connect(self.num9)
+        self.pushButton_50.clicked.connect(self.evaluate)
+        self.pushButton_49.clicked.connect(self.add)
+        self.pushButton_48.clicked.connect(self.subtract)
+        self.pushButton_36.clicked.connect(self.divide)
+        self.pushButton_47.clicked.connect(self.multiply)
+        self.pushButton_46.clicked.connect(self.percentage)
+        self.pushButton_30.clicked.connect(self.back)
+        self.pushButton_32.clicked.connect(self.Clear)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Calculator"))
+        self.pushButton_32.setText(_translate("Form", "Clear"))
+        self.pushButton_39.setText(_translate("Form", "0"))
+        self.pushButton_38.setText(_translate("Form", "."))
+        self.pushButton_33.setText(_translate("Form", "1"))
+        self.pushButton_40.setText(_translate("Form", "2"))
+        self.pushButton_41.setText(_translate("Form", "3"))
+        self.pushButton_34.setText(_translate("Form", "4"))
+        self.pushButton_42.setText(_translate("Form", "5"))
+        self.pushButton_43.setText(_translate("Form", "6"))
+        self.pushButton_35.setText(_translate("Form", "7"))
+        self.pushButton_44.setText(_translate("Form", "8"))
+        self.pushButton_45.setText(_translate("Form", "9"))
+        self.pushButton_46.setText(_translate("Form", "%"))
+        self.pushButton_36.setText(_translate("Form", "÷"))
+        self.pushButton_37.setText(_translate("Form", "00"))
+        self.pushButton_30.setText(_translate("Form", "Back"))
+        self.pushButton_47.setText(_translate("Form", "x"))
+        self.pushButton_48.setText(_translate("Form", "-"))
+        self.pushButton_49.setText(_translate("Form", "+"))
+        self.pushButton_50.setText(_translate("Form", "="))
+        self.label.setText(_translate("Form", " "))
+
+
+    def num1(self):
+       number = self.label.text()
+       self.label.setText(number + "1") 
+    def num2(self):
+       number = self.label.text()
+       self.label.setText(number + "2") 
+    def num3(self):
+       number = self.label.text()
+       self.label.setText(number + "3") 
+    def num4(self):
+       number = self.label.text()
+       self.label.setText(number + "4") 
+    def num5(self):
+       number = self.label.text()
+       self.label.setText(number + "5") 
+    def num6(self):
+       number = self.label.text()
+       self.label.setText(number + "6") 
+    def num7(self):
+       number = self.label.text()
+       self.label.setText(number + "7")               
+    def num8(self):
+       number = self.label.text()
+       self.label.setText(number + "8") 
+    def num9(self):
+       number = self.label.text()
+       self.label.setText(number + "9") 
+    def num0(self):
+       number = self.label.text()
+       self.label.setText(number + "0")
+    def num00(self):
+       number = self.label.text()
+       self.label.setText(number + "00")   
+               
+
+    def evaluate(self):
+        number = self.label.text()
+        try:
+          number = eval(number)  
+          number = str(number)
+          self.label.setText(number)
+        except Exception as e:
+           self.label.setText("Invalid Input")
+
+
+
+    def point(self):
+       number = self.label.text()
+       self.label.setText(number + ".")    
+    def add(self):
+       number = self.label.text()
+       self.label.setText(number + "+")  
+    def subtract(self):
+       number = self.label.text()
+       self.label.setText(number + "-")  
+    def multiply(self):
+       number = self.label.text()
+       self.label.setText(number + "*")  
+    def divide(self):
+       number = self.label.text()
+       self.label.setText(number + "/")
+    def percentage(self):
+       try:
+          number = self.label.text()
+          value = float(number) / 100.0
+          self.label.setText(str(value))
+       except Exception as e:
+          self.label.setText("Error")
+          print("Error:", e)   
+
+    def back(self):
+       number = self.label.text()
+       number = number [:-1]  
+       self.label.setText(number) 
+    def Clear(self):
+        self.label.setText("")
+    
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
